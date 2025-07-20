b0c63b507b1d0cac424f8401fd91b6e4b114189d
rizebutrise <devjatajaa@gmail.com>
2025-07-20 22:23:27 +0300

    решение 2.1.3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,17 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
+
+    public Timer(){
+        this.nanoTime = System.nanoTime();
+        System.out.println("Время:" + nanoTime);
+    }
 
     public Long getTime() {
         return nanoTime;
